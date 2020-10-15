b7fb0fbdaee0f15230db8b741a2fd271cb621d15
14734 <1473441767@qq.com>
2020-10-15 11:11:17 +0800

    @xuhui test Hello Git!

---
+++ src/com/jus/HelloGit.java
@@ -0,0 +1,12 @@
+package com.jus;
+
+/**
+ * @Auther: JusChui
+ * @Date: 2020/10/15 11:06
+ * @Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
